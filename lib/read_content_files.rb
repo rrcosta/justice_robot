4e62fa96a5af20b87e57ce85236fc4d6a62ca35b
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2026-04-04 18:29:25 -0300

    chore: Implements at #adds_left_zero_chars_at_process_number at ReadContentFiles modules

--- lib/read_content_files.rb
+++ lib/read_content_files.rb
@@ -6,6 +6,7 @@ $VERBOSE = nil
 module ReadContentFiles
   extend self
 
+  SIZE_CHARS_NUMBER_PROCESS = 20.freeze
   CONTENT = []
 
   def execute(log, analize_files)
@@ -49,6 +50,21 @@ module ReadContentFiles
   end
 
   def number_process_without_mask(process_number)
-    process_number&.gsub('-','')&.gsub('.','')&.gsub('/','')&.gsub('?','')&.gsub('!','')
+    adds_left_zero_chars_at_process_number(
+      process_number&.gsub('-','')&.gsub('.','')&.gsub('/','')&.gsub('?','')&.gsub('!','')
+    )
+  end
+
+  # Retona o numero do processo com zeros a esquerda ate completar 20 digitos
+  def adds_left_zero_chars_at_process_number(num_proces)
+    size_complement_size = num_proces&.size
+    complement_char      = (SIZE_CHARS_NUMBER_PROCESS - size_complement_size)
+    complement_char = complement_char.negative? ? complement_char&.abs : complement_char
+
+    new_num_proces = "0" * complement_char + num_proces
+
+    new_num_proces
+  rescue
+    num_proces
   end
 end
