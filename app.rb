a4767d7c7811f514e04b07e5bf910c5b260b4162
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2026-04-05 12:42:50 -0300

    chore: Adds calls "CheckDirectories" lib at App.rb

--- app.rb
+++ app.rb
@@ -4,13 +4,24 @@ require 'logger'
 
 require './lib/check_input_files'
 require './lib/read_content_files'
+require './lib/check_directories'
+
+require './services/apis/urls'
+require './services/apis/datajud'
 
 LOGGER = Logger.new($stdout)
 
-status, analize_files = CheckInputFiles.load_files(LOGGER)
+status, _ = CheckDirectories.execute(LOGGER)
 
-status, contents = ReadContentFiles.execute(LOGGER, analize_files) if status
+if status
+  status, analize_files = CheckInputFiles.load_files(LOGGER)
 
-puts contents if status
+  status, contents = ReadContentFiles.execute(LOGGER, analize_files) if status
 
-puts ENV.fetch('TOKEN_API', 'Nao carregado')
+  status, contents = ::Apis::Datajud.call(LOGGER, contents) if status
+
+else
+  msg = "Não foi possivel Criar OU Ler os arquivos do diretório ENTRADA"
+  LOGGER.error(msg)
+  puts mgs
+end
