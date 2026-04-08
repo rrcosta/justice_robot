45b56c00145674a27d7c7e280653825a012da9f3
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2026-04-08 17:38:27 -0300

    chore:  Calls stact below at app.rb - ::PreparedFiles::CheckDirectories - ::PreparedFiles::CheckInputFiles - ::PreparedFiles::ReadContentFiles - ::Service::Apis::Datajud::Call

--- app.rb
+++ app.rb
@@ -9,6 +9,8 @@ require './lib/prepared_files/check_directories'
 require './service/apis/datajud/urls'
 require './service/apis/datajud/call'
 
+require './service/apis/datajud/parse'
+
 # Disable warning messages
 $VERBOSE = nil
 
@@ -22,6 +24,16 @@ if status
   status, contents = ::PreparedFiles::ReadContentFiles.execute(LOGGER, files) if status
 
   status, contents = ::Service::Apis::Datajud::Call.execute(LOGGER, contents) if status
+
+  # TODO
+  # A partir deste momento, o ideal seria efetuar em backgroud job OU temporalIO
+  # Parse --> ::Service::Apis::Datajud::Parse.execute(content)
+  contents.each do |content|
+    pre_xls = ::Service::Apis::Datajud::Parse.execute(content)
+
+    puts pre_xls
+  end
+
 else
   msg = "Não foi possivel Criar OU Ler os arquivos do diretório ENTRADA. Detalhes: #{err_check_directories[:error]}"
   LOGGER.error(msg)
