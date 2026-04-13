22870b6a20251d00403d4bd0f29fe636c63ab687
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2026-04-13 15:20:47 -0300

    chore: Introduces spec_helper files below spec folders

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+
+FOLDERS_MAP = -> (folders) do
+  File.join(File.dirname(__FILE__), "#{folders}", "**/*.rb")
+end
+
+RSpec.configure do |config|
+  config.order = 'random'
+
+  Dir[FOLDERS_MAP.call('lib')].each { |f| require f }
+  Dir[FOLDERS_MAP.call('service')].each { |f| require f }
+end
